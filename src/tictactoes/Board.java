0c2eb3b82fb5afd7eb0618ce2596652ba94cc6fc
Keiliah McDonald <mcdonaldk1@wit.edu>
2024-04-09 16:57:45 -0400

    ugg

--- src/tictactoes/Board.java
+++ src/tictactoes/Board.java
@@ -7,7 +7,7 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+import javafx.scene.Parent;	
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
