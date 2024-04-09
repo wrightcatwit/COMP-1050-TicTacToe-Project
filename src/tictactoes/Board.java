cf57e75a01a5739c24d50d50685d724d8710bc4d
Keiliah McDonald <mcdonaldk1@wit.edu>
2024-04-09 16:05:52 -0400

    e

--- src/tictactoes/Board.java
+++ src/tictactoes/Board.java
@@ -1,25 +1,39 @@
-package tictactoes;
+	package tictactoes;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Board extends Application {
+public class Board extends Application implements Initializable{
 	
-	private int[][] Secs = new int[3][3]; 
+	@FXML
+	GridPane o1 = new GridPane();
 	
 	public static void main(String[] args) {launch(args);}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		
 		Parent root = FXMLLoader.load(getClass().getResource("TicTacToeUI.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setTitle("TicTacToes");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
 		
 	}
 	
