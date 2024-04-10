cb0471bad580ad4a7c04ce11762183cd4e0825a9
Keiliah McDonald <mcdonaldk1@wit.edu>
2024-04-10 15:21:13 -0400

    Made the buttons correct and now everything on the boards click

--- src/tictactoes/Board.java
+++ src/tictactoes/Board.java
@@ -218,19 +218,62 @@ public class Board extends Application implements Initializable{
 //
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		b1Buttons = new ArrayList<>(Arrays.asList(b1r1c1, b1r2c1, b1r3c1, b1r2c1, b1r2c2, b1r2c3, b1r3c1, b1r3c2, b1r3c3));
-		b2Buttons = new ArrayList<>(Arrays.asList(b2r1c1, b2r2c1, b2r3c1, b2r2c1, b2r2c2, b2r2c3, b2r3c1, b2r3c2, b2r3c3));
-		b3Buttons = new ArrayList<>(Arrays.asList(b3r1c1, b3r2c1, b3r3c1, b3r2c1, b3r2c2, b3r2c3, b3r3c1, b3r3c2, b3r3c3));
-		b4Buttons = new ArrayList<>(Arrays.asList(b4r1c1, b4r2c1, b4r3c1, b4r2c1, b4r2c2, b4r2c3, b4r3c1, b4r3c2, b4r3c3));
-		b5Buttons = new ArrayList<>(Arrays.asList(b5r1c1, b5r2c1, b5r3c1, b5r2c1, b5r2c2, b5r2c3, b5r3c1, b5r3c2, b5r3c3));
-		b6Buttons = new ArrayList<>(Arrays.asList(b6r1c1, b6r2c1, b6r3c1, b6r2c1, b6r2c2, b6r2c3, b6r3c1, b6r3c2, b6r3c3));
-		b7Buttons = new ArrayList<>(Arrays.asList(b7r1c1, b7r2c1, b7r3c1, b7r2c1, b7r2c2, b7r2c3, b7r3c1, b7r3c2, b7r3c3));
-		b8Buttons = new ArrayList<>(Arrays.asList(b8r1c1, b8r2c1, b8r3c1, b8r2c1, b8r2c2, b8r2c3, b8r3c1, b8r3c2, b8r3c3));
-		b9Buttons = new ArrayList<>(Arrays.asList(b9r1c1, b9r2c1, b9r3c1, b9r2c1, b9r2c2, b9r2c3, b9r3c1, b9r3c2, b9r3c3));
+		b1Buttons = new ArrayList<>(Arrays.asList(b1r1c1, b1r1c2, b1r1c3, b1r2c1, b1r2c2, b1r2c3, b1r3c1, b1r3c2, b1r3c3));
+		b2Buttons = new ArrayList<>(Arrays.asList(b2r1c1, b2r1c2, b2r1c3, b2r2c1, b2r2c2, b2r2c3, b2r3c1, b2r3c2, b2r3c3));
+		b3Buttons = new ArrayList<>(Arrays.asList(b3r1c1, b3r1c2, b3r1c3, b3r2c1, b3r2c2, b3r2c3, b3r3c1, b3r3c2, b3r3c3));
+		b4Buttons = new ArrayList<>(Arrays.asList(b4r1c1, b4r1c2, b4r1c3, b4r2c1, b4r2c2, b4r2c3, b4r3c1, b4r3c2, b4r3c3));
+		b5Buttons = new ArrayList<>(Arrays.asList(b5r1c1, b5r1c2, b5r1c3, b5r2c1, b5r2c2, b5r2c3, b5r3c1, b5r3c2, b5r3c3));
+		b6Buttons = new ArrayList<>(Arrays.asList(b6r1c1, b6r1c2, b6r1c3, b6r2c1, b6r2c2, b6r2c3, b6r3c1, b6r3c2, b6r3c3));
+		b7Buttons = new ArrayList<>(Arrays.asList(b7r1c1, b7r1c2, b7r1c3, b7r2c1, b7r2c2, b7r2c3, b7r3c1, b7r3c2, b7r3c3));
+		b8Buttons = new ArrayList<>(Arrays.asList(b8r1c1, b8r1c2, b8r1c3, b8r2c1, b8r2c2, b8r2c3, b8r3c1, b8r3c2, b8r3c3));
+		b9Buttons = new ArrayList<>(Arrays.asList(b9r1c1, b9r1c2, b9r1c3, b9r2c1, b9r2c2, b9r2c3, b9r3c1, b9r3c2, b9r3c3));
 		
 	//making 9 of these later
 		b1Buttons.forEach(b -> {
-			pressed(b);
+			pressed(b,b1Buttons);
+			b.setFocusTraversable(false);
+		});
+		
+		b2Buttons.forEach(b -> {
+			pressed(b,b2Buttons);
+			b.setFocusTraversable(false);
+		});
+		
+		b3Buttons.forEach(b -> {
+			pressed(b,b3Buttons);
+			b.setFocusTraversable(false);
+		});
+		
+		b4Buttons.forEach(b -> {
+			pressed(b,b4Buttons);
+			b.setFocusTraversable(false);
+		});
+		
+		
+		b5Buttons.forEach(b -> {
+			pressed(b,b5Buttons);
+			b.setFocusTraversable(false);
+		});
+		
+		
+		b6Buttons.forEach(b -> {
+			pressed(b,b6Buttons);
+			b.setFocusTraversable(false);
+		});
+		
+		
+		b7Buttons.forEach(b -> {
+			pressed(b,b7Buttons);
+			b.setFocusTraversable(false);
+		});
+		
+		b8Buttons.forEach(b -> {
+			pressed(b,b8Buttons);
+			b.setFocusTraversable(false);
+		});
+		
+		b9Buttons.forEach(b -> {
+			pressed(b,b9Buttons);
 			b.setFocusTraversable(false);
 		});
 	}
@@ -246,7 +289,7 @@ public class Board extends Application implements Initializable{
 	}
 	
 	//i want to switch this to take an array of all the button ones
-	public void gameOver() {
+	public void gameOver(ArrayList<Button> e) {
 		/*for(int i = 0; i < 8; i++) {
 			String line = switch(i) {
 			case 0 -> but1.getText() + but2.getText() + but3.getText();
@@ -268,11 +311,11 @@ public class Board extends Application implements Initializable{
 		}*/
 	}
 	
-	private void pressed(Button b) {
+	private void pressed(Button b, ArrayList<Button> e) {
 		b.setOnMouseClicked(m -> {
 			setSymbol(b);
 			b.setDisable(true);
-			gameOver();
+			gameOver(e);
 		});
 	}
 
