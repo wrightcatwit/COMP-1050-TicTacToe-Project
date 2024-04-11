1a17779ed2bce42045c94c9b39c8552162feeb05
Keiliah McDonald <mcdonaldk1@wit.edu>
2024-04-11 15:03:49 -0400

    test 4

--- src/tictactoes/Board.java
+++ src/tictactoes/Board.java
@@ -444,12 +444,12 @@ public class Board extends Application implements Initializable{
 				u.setText("X");
 				l.setVisible(false);
 				player = 0;
-				turn.setText("X won, X's turn");
+				turn.setText("X won, X goes first!");
 			}else if(line.equals("OOO")) {
 				u.setText("O");
 				l.setVisible(false);
 				player = 1;
-				turn.setText("O won, O's turn");
+				turn.setText("O won, O goes first!");
 			}
 		
 			
